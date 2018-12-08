d77f3a68d70193cd5c65f3f779a71520c50c9d56
Juan Pablo <juanpabloguleal@gmail.com>
2018-12-08 01:44:20 -0600

    First commit

---
+++ admSoftware/Prim.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace admSoftware
+{
+    partial class Prim
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(497, 320);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Tag = "dataGridView1";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.MinimumWidth = 100;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 160;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Direccion/Path";
+            this.Column2.Name = "Column2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox3.Location = new System.Drawing.Point(530, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(248, 16);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Nombre PC";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox4.Location = new System.Drawing.Point(529, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(249, 16);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Sistema Operativo";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(530, 187);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(247, 50);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(529, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(249, 47);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(497, 24);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "Software instalado en este sistema:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(12, 7);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(497, 13);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "Matricula: 1674335       Estudiante: Juan Pablo Guerrero Leal";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 74);
+            this.button1.TabIndex = 10;
+            this.button1.Tag = "button1";
+            this.button1.Text = "Generar informacion en PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(530, 283);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(248, 13);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Tag = "textBox5";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Prim
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Prim";
+            this.Text = "Administrador de Software";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ admSoftware/Prim.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Diagnostics;
+
+namespace admSoftware
+{
+    public partial class Prim : Form
+    {
+        int contador = 0;
+        String nombPC;
+        String syst;
+        public Prim()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            SoftwareEnSistema();
+            InformacionGeneral();
+        }
+        private void SoftwareEnSistema()
+        {
+            Object nombre;
+            Object path;
+            String snombre;
+            String spath;
+            RegistryKey MiReg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+            string[] subkeys = MiReg.GetSubKeyNames();
+
+            DataTable datGridView1 = new DataTable();
+            datGridView1.Columns.Add("Nombre");
+            datGridView1.Columns.Add("Direccion/Path");
+            foreach (string subkey in subkeys)
+            {
+                MiReg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\"+subkey);
+                nombre = MiReg.GetValue("DisplayName");
+                if(nombre == null)
+                {
+                    snombre = subkey.ToString();
+                }
+                else { 
+                    snombre = nombre.ToString();
+                }
+                path = MiReg.GetValue("InstallLocation");
+                if(path == null)
+                {
+                    path = MiReg.GetValue("InstallSource");
+                    if (path == null)
+                    {
+                        path = MiReg.GetValue("Inno Setup: App Path");
+                        if(path == null)
+                        {
+                            path = MiReg.GetValue("InstallDir");
+                            
+                            if(path == null)
+                            {
+                                path = MiReg.GetValue("DisplayIcon");
+                                if (path == null)
+                                {
+                                    spath = "Null";
+                                }
+                                else
+                                {
+                                    spath = path.ToString();
+                                }
+                            }
+                            else
+                            {
+                                spath = path.ToString();
+                            }
+                        }
+                        else
+                        {
+                            spath = path.ToString();
+                        }
+                    }
+                    else
+                    {
+                        spath = path.ToString();
+                    }
+                }
+                else
+                {
+                    spath = path.ToString();
+                }
+ 
+                DataRow row = datGridView1.NewRow();
+                row["Nombre"] = snombre;
+                row["Direccion/Path"] = spath;
+                datGridView1.Rows.Add(row);
+            }
+
+            foreach (DataRow Drow in datGridView1.Rows) {
+                int num = dataGridView1.Rows.Add();
+                dataGridView1.Rows[num].Cells[0].Value = Drow["Nombre"].ToString();
+                dataGridView1.Rows[num].Cells[1].Value = Drow["Direccion/Path"].ToString();
+            }
+        }
+
+        public void InformacionGeneral()
+        {
+            Object nomPC = new Object();
+            Object sy = new Object(); ;
+            RegistryKey MiReg = Registry.CurrentUser.OpenSubKey("Volatile Environment");
+            nomPC = MiReg.GetValue("USERDOMAIN");
+            MiReg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+            sy = MiReg.GetValue("ProductName");
+            nombPC = nomPC.ToString();
+            syst = sy.ToString();
+            richTextBox1.Text = nombPC;
+            richTextBox2.Text = syst;
+        }
+
+        public void GenerarPDF()
+        {
+            FileStream fs = new FileStream("Reporte.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+            doc.Open();
+            doc.NewPage();
+            doc.Add(new Paragraph("Generado con admSoftware. Software por Juan Pablo Guerrero Leal. Matricula 1674335\n\n"));
+            doc.Add(new Paragraph("A continuacion se presenta en forma de tabla el listado de software instalados en el sistema y sus respectivas rutas.\n\n"));
+            doc.Add(new Paragraph(""));
+
+
+            PdfPTable table1 = new PdfPTable(1);
+            table1.DefaultCell.Border = 0;
+            table1.WidthPercentage = 80;
+            table1.DefaultCell.BorderWidth = 2;
+            table1.DefaultCell.HorizontalAlignment = Element.ALIGN_CENTER;
+
+
+            PdfPCell cell11 = new PdfPCell();
+            cell11.Colspan = 1;
+            cell11.AddElement(new Paragraph("Nombre PC: "+nombPC));
+
+            cell11.AddElement(new Paragraph("S.O.: "+syst));
+            cell11.VerticalAlignment = Element.ALIGN_CENTER;
+            cell11.HorizontalAlignment = Element.ALIGN_CENTER;
+            PdfPCell cell12 = new PdfPCell();
+
+
+            cell12.VerticalAlignment = Element.ALIGN_CENTER;
+            cell12.HorizontalAlignment = Element.ALIGN_CENTER;
+
+
+            table1.AddCell(cell11);
+
+            table1.AddCell(cell12);
+
+
+            PdfPTable table2 = new PdfPTable(2);
+
+            PdfPCell cell21 = new PdfPCell();
+
+            cell21.AddElement(new Paragraph("Nombre"));
+
+            PdfPCell cell22 = new PdfPCell();
+
+            cell22.AddElement(new Paragraph("Direccion/Path"));
+
+
+
+            table2.AddCell(cell21);
+
+            table2.AddCell(cell22);
+
+            Object nombre;
+            Object path;
+            String snombre;
+            String spath;
+            RegistryKey MiReg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+            string[] subkeys = MiReg.GetSubKeyNames();
+            foreach (string subkey in subkeys)
+            {
+                MiReg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + subkey);
+                nombre = MiReg.GetValue("DisplayName");
+                if (nombre == null)
+                {
+                    snombre = subkey.ToString();
+                }
+                else
+                {
+                    snombre = nombre.ToString();
+                }
+                path = MiReg.GetValue("InstallLocation");
+                if (path == null)
+                {
+                    path = MiReg.GetValue("InstallSource");
+                    if (path == null)
+                    {
+                        path = MiReg.GetValue("Inno Setup: App Path");
+                        if (path == null)
+                        {
+                            path = MiReg.GetValue("InstallDir");
+
+                            if (path == null)
+                            {
+                                path = MiReg.GetValue("DisplayIcon");
+                                if (path == null)
+                                {
+                                    spath = "Null";
+                                }
+                                else
+                                {
+                                    spath = path.ToString();
+                                }
+                            }
+                            else
+                            {
+                                spath = path.ToString();
+                            }
+                        }
+                        else
+                        {
+                            spath = path.ToString();
+                        }
+                    }
+                    else
+                    {
+                        spath = path.ToString();
+                    }
+                }
+                else
+                {
+                    spath = path.ToString();
+                }
+                PdfPCell cell = new PdfPCell();
+                cell.AddElement(new Paragraph(snombre));
+                cell.FixedHeight = 50.0f;
+                PdfPCell cell2 = new PdfPCell();
+                cell2.AddElement(new Paragraph(spath));
+                cell2.FixedHeight = 50.0f;
+                table2.AddCell(cell);
+                table2.AddCell(cell2);
+                
+            }
+
+            doc.Add(table1);
+            doc.Add(table2);
+            Paragraph parrafo1 = new Paragraph("\n\n\n\nFirme aqui de enterado:\n\n\n\n");
+            parrafo1.Alignment = Element.ALIGN_LEFT;
+            doc.Add(parrafo1);
+            Paragraph parrafo2 = new Paragraph("____________________");
+            doc.Add(parrafo2);
+            parrafo2.Alignment = Element.ALIGN_LEFT;
+
+            doc.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (contador == 0)
+            {
+                GenerarPDF();
+                textBox5.Text = "Archivo Generado. Encuentralo en la carpeta raiz. ";
+                contador++;
+                string pdfPath = Path.Combine(Application.StartupPath, "Reporte.pdf");
+                Process.Start(pdfPath);
+            }
+            else
+            {
+                textBox5.Text = "El archivo ya ha sido generado.";
+            }
+           
+            
+        }
+    }
+}
